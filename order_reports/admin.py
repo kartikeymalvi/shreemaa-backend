14746dda63057e220bdee6335b3ebce6e1ec5f65
kartikeymalvi <kartikeymalviya02@gmail.com>
2026-06-17 16:02:35 +0530

    fixed error in admin

--- order_reports/admin.py
+++ order_reports/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import OrderReport, Firm, Location, Merchant, ColumnVisibilityPolicy
+from .models import CustomUser
 
 # In sabko admin panel me dikhane ke liye register karna padta hai
 admin.site.register(CustomUser)
