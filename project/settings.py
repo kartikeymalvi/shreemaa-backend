480d6d31a1e79257f1535f79ce855ad618651557
kartikeymalvi <kartikeymalviya02@gmail.com>
2026-07-17 23:42:56 +0530

    updated coresheaders

--- project/settings.py
+++ project/settings.py
@@ -185,7 +185,9 @@ CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",  
     "http://localhost:5173",  
-    "http://127.0.0.1:5500",  
+    "http://127.0.0.1:5500", 
+    "http://localhost:5173",
+    "https://smg-erp.duckdns.org",
     "https://shreemaa-frontend.vercel.app",
 ]
 
