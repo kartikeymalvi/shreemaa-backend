637672b037ad35c5ee42dd980a841febd65bd0e6
kartikeymalvi <kartikeymalviya02@gmail.com>
2026-07-19 23:29:56 +0530

    updated timezone Asia/kolkata

--- project/settings.py
+++ project/settings.py
@@ -247,6 +247,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'Asia/Kolkata'
+USE_I18N = True
 USE_TZ = True
 
 # 🔥 UPDATE 5: Render par Static Files (CSS/JS) serve karne ka system
